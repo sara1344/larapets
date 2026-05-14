e4fb3f17547a9ecf606816b14a4b8197b44d07dd
sara1344 <marquezsara1811@gmail.com>
2026-05-13 20:09:24 -0500

    larapets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    {{-- <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" /> --}}
-    <link href="{{asset('css/daisyui5.css')}}" rel="stylesheet" type="text/css" />
-    {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
-    <script src={{asset('js/tailwindcss4.js')}}></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    {{--  <link href="{{asset('css/daisyui5.css')}}" rel="stylesheet" type="text/css" /> --}}
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    {{--  <script src={{asset('js/tailwindcss4.js')}}></script> --}}
     <script src={{asset('js/jquery-3.7.1.min.js')}}></script>
     <script src={{asset('js/sweetalert2@11.js')}}></script>
 </head>
